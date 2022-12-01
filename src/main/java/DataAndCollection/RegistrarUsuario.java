4a5ff6c57a3a7d7770217551e73a4f59a553151b
Manuel <72231982+SchustentUFRO@users.noreply.github.com>
2022-12-01 11:55:53 -0300

    cambios se quita todo lo de swing

---
+++ src/main/java/DataAndCollection/RegistrarUsuario.java
@@ -0,0 +1,15 @@
+package DataAndCollection;
+
+public class RegistrarUsuario {
+    private String user;
+    private String password;
+
+    public RegistrarUsuario (String user, String password){
+        this.user=user;
+        this.password=password;
+    }
+
+    public void registrarUsuario(){
+
+    }
+}
--- src/main/java/guiLogin/guiDesing.java
+++
@@ -1,7 +0,0 @@
-package guiLogin;
-
-import javax.swing.*;
-
-public class guiDesing {
-    private JButton button1;
-}
