7858ae7f44b29f156bf0b08ec2c6a9445c076166
DiegoArroyo1 <arroyi2000@gmail.com>
2022-12-03 23:15:45 -0300

    adicion para registrar usuario y login

---
+++ src/main/java/GUI/LoginPanel.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class LoginPanel {
+    private JTextField textField9;
+    private JTextField textField1;
+    private JButton registrarButton;
+    private JButton iniciarSesiónButton;
+}
